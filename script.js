326a9c5bb155f244bd807de18208ce5904ae100d
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2025-01-08 04:05:18 -0300

    Changed promises for async-await, added try-catch

--- script.js
+++ script.js
@@ -4,30 +4,24 @@ const imgContainer = document.querySelector(".img-container");
 const form = document.querySelector("form");
 const searchContent = document.querySelector(".search");
 
-function fetchImg(search = "cats") {
-    fetch(
-        `https://api.giphy.com/v1/gifs/translate?api_key=QYDcK66FzounkHbq2pLGzUetmZ9GyzyH&s=${search}`,
-        { mode: "cors" }
-    )
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function (response) {
-            if(response.data.length === 0) return
-            searchContent.value = ""
-            console.log(img.src);
-            console.log(response.data);
-            imgContainer.classList.remove("hidden");
-            if (img.src === response.data.images.original.url) {
-                fetchImg();
-                return;
-            }
-            img.src = response.data.images.original.url;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+async function fetchImg(search = "cats") {
+    try {
+        const response = await fetch(
+            `https://api.giphy.com/v1/gifs/translate?api_key=QYDcK66FzounkHbq2pLGzUetmZ9GyzyH&s=${search}`,
+            { mode: "cors" }
+        );
+        const gif = await response.json();
+        searchContent.value = "";
+        if (gif.data.length === 0) throw new Error("Empty String");
+        if (img.src === gif.data.images.original.url) {
+            fetchImg();
+            return;
+        }
+        img.src = gif.data.images.original.url;
+        imgContainer.classList.remove("hidden");
+    } catch (error) {
+        console.error(error);
+    }
 }
 cat.addEventListener("click", (e) => {
     fetchImg();
@@ -35,5 +29,32 @@ cat.addEventListener("click", (e) => {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     fetchImg(searchContent.value);
-    console.log(searchContent.value);
 });
+
+//- Promise based version -
+
+// function fetchImg(search = "cats") {
+//     fetch(
+//         `https://api.giphy.com/v1/gifs/translate?api_key=QYDcK66FzounkHbq2pLGzUetmZ9GyzyH&s=${search}`,
+//         { mode: "cors" }
+//     )
+//         .then(function (response) {
+//             console.log(response);
+//             return response.json();
+//         })
+//         .then(function (response) {
+//             if(response.data.length === 0) return
+//             searchContent.value = ""
+//             console.log(img.src);
+//             console.log(response.data);
+//             imgContainer.classList.remove("hidden");
+//             if (img.src === response.data.images.original.url) {
+//                 fetchImg();
+//                 return;
+//             }
+//             img.src = response.data.images.original.url;
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
